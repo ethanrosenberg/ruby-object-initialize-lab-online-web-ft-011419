fa104eb3f71442648020ea41063c6366a141e4ee
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-01-23 01:12:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class Dog
   def initialize(dog_name, dog_breed)
     @name = dog_name
     
-    if dog_breed != nil
+    if dog_breed != ""
       @breed = dog_breed
     else
       @breed = "Mutt"
